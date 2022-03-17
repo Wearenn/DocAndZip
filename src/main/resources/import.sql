dc53fdc9dc2c109d9ed746e092e047eb6b71494e
wearenn <sebastien.philipp57@gmail.com>
2022-03-17 23:04:43 +0100

    changement jeu de données

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,6 +5,6 @@ INSERT INTO "type" ("type_id", "type") VALUES (4, 'binaire');
 
 INSERT INTO "document" ("nom", "chemin", "type_id", "date_ajout") VALUES ('cv', '~/Documents/Tests/cv.txt', 1, '2022-03-14');
 INSERT INTO "document" ("nom", "chemin", "type_id", "date_ajout") VALUES ('memoire d alternance', '~/Documents/Tests/memoire_alternance.txt', 1, '2022-03-14');
-INSERT INTO "document" ("nom", "chemin", "type_id", "date_ajout") VALUES ('vivien est chiant', '~/Documents/Tests/vivien.exe', 4, '2022-03-14');
+INSERT INTO "document" ("nom", "chemin", "type_id", "date_ajout") VALUES ('vivien.exe', '~/Documents/Tests/vivien.exe', 4, '2022-03-14');
 INSERT INTO "document" ("nom", "chemin", "type_id", "date_ajout") VALUES ('cours CGL', '~/Documents/Tests/cours.avi', 3, '2022-03-14');
 INSERT INTO "document" ("nom", "chemin", "type_id", "date_ajout") VALUES ('sous le vent', '~/Documents/Tests/garou.mp3', 2, '2022-03-14');
